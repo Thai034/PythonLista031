a8f54319ea2e060cfc3eec6299505da8e181fd4a
Thaila Alves <thailasalvees@gmail.com>
2023-08-02 10:49:23 -0300

    exercicio 1 e 2 prontos

---
+++ questao01.py
@@ -0,0 +1,6 @@
+print("Olá, Por favor informe seus dados:")
+nome = (input("Nome completo: "))
+nome2 = (input("Sexo: "))
+
+print("nome do usuário: ", nome)
+print("sexo do usuário: ", nome2)
---
+++ questao02.py
@@ -0,0 +1,10 @@
+print("insira abaixo seus números para que calculemos a soma e multiplicação")
+num1 = float(input("Informe o primeiro número: "))
+num2 = float(input("Informe o segundo número: "))
+num3 = float(input("Informe o terceiro número: "))
+
+resul1 = num1 + num2 + num3
+resul2 = num1 * num2 * num3
+
+print("Resultado da soma: ", resul1)
+print("Resultado da multiplicação: ", resul2)
