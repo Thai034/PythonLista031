148b32ed008e990be41b4d6edbdc16c4c115ddc4
Thaila Alves <thailasalvees@gmail.com>
2023-08-07 11:35:16 -0300

    Muitos arquivos novos

---
+++ interpolacao_ strings/interpolacao_string_%.py
@@ -0,0 +1,14 @@
+nome = "Carla Joaquina" #tipo str
+idade = 27 # tipo int
+altura = 1.759 # tipo float
+
+#A função type é string
+
+print(f"Tipo da var nome: %s " % type(nome))
+print(f"Tipo da var idade: %s " % type(idade))
+print(f"Tipo da var altura: %s " % type(altura))
+
+print("Nome: %s" % nome)
+print("Idade: %d" % idade)
+print("Altura: %.2f" % altura)
+print("%s possui %d anos e tem %.2fm de altura" % (nome, idade, altura))
